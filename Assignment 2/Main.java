44d09ffb35259468b0a29cb54a776656d354f7b6
Axxelium <59525687+Axxelium@users.noreply.github.com>
2026-09-20 21:15:30 +0500

    Add factory method and abstract factory demonstrations
    Implemented factory method and abstract factory patterns to demonstrate weapon and equipment creation.

--- Assignment 2/Main.java
+++ Assignment 2/Main.java
@@ -1,5 +1,24 @@
-public class Main {
-    public void main(String[] args) {
+import factories.*;
 
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("1. FACTORY METHOD DEMONSTRATION");
+        WeaponFactory swordMaker = new SwordFactory();
+        swordMaker.prepareAndInspectWeapon();
+
+        System.out.println("\n---------");
+        WeaponFactory bowMaker = new BowFactory();
+        bowMaker.prepareAndInspectWeapon();
+
+        System.out.println("\n2. ABSTRACT FACTORY DEMONSTRATION");
+        System.out.println("Deploying Warrior Loadout:");
+        EquipmentFactory warriorKit = new WarriorEquipmentFactory();
+        HeroEquipmentManager warrior = new HeroEquipmentManager(warriorKit);
+        warrior.enterCombat();
+
+        System.out.println("\nDeploying Archer Loadout:");
+        EquipmentFactory archerKit = new ArcherEquipmentFactory();
+        HeroEquipmentManager archer = new HeroEquipmentManager(archerKit);
+        archer.enterCombat();
     }
 }
